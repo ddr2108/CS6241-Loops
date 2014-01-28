8fc7f7c8e543a0f4671e0cf5002475b6c7c7a1a7
Deep Datta Roy <deepdattaroy8888@gmail.com>
2014-01-27 22:12:10 -0500

    Did Part 3 analysis

--- Test Code/hello.c
+++ Test Code/hello.c
@@ -19,9 +19,15 @@ int main() {
   second();
 int i;
 while(rand()%1000!=3);
-//{while(rand()%1000!=3);}
+{
+i++;
+while(rand()%5!=3){
+	if (i==4){
+		break;
+}
+}}
 
   return 0;
 }
 
-
+//clang hello.c -S -emit-llvm; opt -dot-cfg hello.ll;dot -Tpng cfg.main.dot > main.png
